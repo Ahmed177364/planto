001855da28f564a366ea4fda5d3b481cdccedf5c
Ahmed177364 <tuopeerror@gmail.com>
2024-09-03 22:57:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var close = document.querySelector('.sidehedtopheader');
+var spe = document.querySelector('.speac');
+var hed = document.querySelector('.header');
+
+function mybutton() {
+    close.style.display = "none";
+    spe.style.height = "90px";
+    hed.style.marginTop = "0px";
+}
